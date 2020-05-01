63ed33dae12140ee760c975bc89b9c30f7c34dfd
Robynn McCrossan <2483898m@student.gla.ac.uk>
2020-05-01 12:39:07 +0100

    actually chapter 6

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -10,27 +10,35 @@ def populate():
 
         python_pages = [
             {'title': 'Official Python Tutorial',
-             'url': 'http://docs.python.org/3/tutorial/'},
+             'url': 'http://docs.python.org/3/tutorial/',
+             'views': 18},
             {'title': 'How to Think like a Computer Scientist',
-             'url': 'http://www.greeenteapress.com/thinkpython/'},
+             'url': 'http://www.greeenteapress.com/thinkpython/',
+             'views': 168},
             {'title': 'Learn Python in 10 Minutes',
-             'url': 'http://www.korokithakis.net/tutorials/python'}
+             'url': 'http://www.korokithakis.net/tutorials/python',
+             'views': 128}
         ]
 
         django_pages = [
             {'title': 'Official Django Tutorial',
-             'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+             'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+             'views': 128},
             {'title': 'Django Rocks',
-             'url': 'http://www.djangorocks.com/'},
+             'url': 'http://www.djangorocks.com/',
+             'views': 28},
             {'title': 'How to Tango with Django',
-             'url': 'http://www.tangowithdjango.com/'}
+             'url': 'http://www.tangowithdjango.com/',
+             'views': 128}
         ]
 
         other_pages = [
             {'title': 'Bottle',
-             'url': 'http://bottlepy.org/docs/dev/'},
+             'url': 'http://bottlepy.org/docs/dev/',
+             'views': 118},
             {'title': 'Flask',
-             'url': 'http://flask.pocoo.org'}
+             'url': 'http://flask.pocoo.org',
+             'views': 108}
         ]
 
         cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
@@ -40,14 +48,14 @@ def populate():
         for cat, cat_data in cats.items():
             c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
             for p in cat_data['pages']:
-                add_page(c, p['title'], p['url'])
+                add_page(c, p['title'], p['url'], p['views'])
 
         for c in Category.objects.all():
             for p in Page.objects.filter(category=c):
                 print(f'-{c}:{p}')
 
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p=Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -6,6 +6,10 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+
 admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CategoryAdmin)
 
