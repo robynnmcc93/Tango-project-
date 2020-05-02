eba3db9bf69062c8f750b2042104653fe7f094fe
Robynn McCrossan <2483898m@student.gla.ac.uk>
2020-05-02 09:38:40 +0100

    chapter 7 finished with all tests passing

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -55,7 +55,7 @@ def populate():
                 print(f'-{c}:{p}')
 
 
-def add_page(cat, title, url, views):
+def add_page(cat, title, url, views=0):
     p=Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
